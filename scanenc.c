afce817105016f44fda2df68000da7b4724f4e07
Robert Mibus <mibus@mibus.org>
2000-10-06 11:17:30 +0000

    added the license header

--- scanenc.c
+++ scanenc.c
@@ -1,3 +1,27 @@
+/*****************************************************************************/
+/* star trek ency reader: Reads the Star Trek Encyclopedia (1998 version)    */
+/* Also reads the various Omnipedias & Episode guides                        */
+/* Copyright (C) 1998 Robert Mibus                                           */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or (at your option) any later version.                    */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.*/
+/*                                                                           */
+/* Author:                                                                   */
+/*      Email   mibus@bigpond.com                                            */
+/*      Webpage http://users.bigpond.com/mibus/                              */
+/*****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
