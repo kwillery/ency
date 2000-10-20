8f6b50a3aecf5ebce4a9f9aec8965876ab7d254e
Robert Mibus <mibus@mibus.org>
2000-10-20 10:49:48 +0000

    Add support for scanning for picture tables/captions

--- scanenc.c
+++ scanenc.c
@@ -34,6 +34,164 @@
 int tell = 0;
 long old_ftell;
 
+int is_all_ascii (char *string)
+{
+	while (*string)
+		if (isascii (*string++) == 0) return 0;
+	return 1;
+}
+
+int ends_in_number (char *string, int expected_length)
+{
+	return (isdigit(string[expected_length]));
+}
+int ends_in_quote (char *string, int expected_length)
+{
+	return ((string[1] == '\"') || (string[expected_length] == '\"'));
+}
+
+int not_embedding_bracket (char *string)
+{
+	return ((string[0] != '[') && (string[1] != '['));
+}
+
+int doesnt_have_junk (char *string)
+{
+	return (isalpha (string[0]) || isalpha (string[1]) || ispunct (string[0]) || ispunct (string[1]));
+}
+
+void check_for_captions (FILE *inp, FILE *outp)
+{
+	char c=0;
+	char fnbase[9]="        ";
+	char temp[9] = "        ";
+	long found_at;
+
+	found_at = ftell (inp);
+
+	fread (fnbase, 8, 1, inp);
+	st_cleanstring (fnbase);
+
+	if (is_all_ascii (fnbase) && ends_in_quote (fnbase, 7) && ends_in_number (fnbase, 6))
+	{
+		if (fnbase[1] == '\"')
+			fseek (inp, found_at + 2, SEEK_SET);
+
+		if (getc (inp) == ':')
+		{
+			fread (temp, 8, 1, inp);
+			if (not_embedding_bracket(temp))
+			{
+				if (doesnt_have_junk(temp))
+				{
+					*strchr (fnbase, '\"') = 0;
+
+					fseek (inp, found_at, SEEK_SET);
+					while ((c != ']') && (c != '\"'))
+						c = getc (inp);
+					if (c == ']')
+						return;
+					while ((c = getc (inp)) != '\"')
+						;
+					while ((c = getc (inp)) != '\"')
+						;
+					while ((c = getc (inp)) != '\"')
+						;
+
+					fread (temp, 8, 1, inp);
+					if (!is_all_ascii (temp))
+						return;
+					if (!ends_in_quote (temp, 7))
+						return;
+					if (!ends_in_number (temp, 6))
+						return;
+
+					printf ("found cpt @ 0x%lx\t%s\n", found_at, fnbase);
+					if (outp)
+						fprintf (outp, "found cpt @ 0x%lx\t%s\n", found_at, fnbase);
+					fseek (inp, found_at, SEEK_SET);
+					return;
+				}
+			}
+		}
+	}
+	fseek (inp, found_at, SEEK_SET);
+}
+
+int check_for_table (FILE *inp, FILE *outp)
+{
+	char c=0;
+	char fnbase[8]="       ";
+	char temp[8] = "       ";
+	long found_at;
+
+	found_at = ftell (inp);
+
+	fread (fnbase, 7, 1, inp);
+	st_cleanstring (fnbase);
+
+	if (is_all_ascii (fnbase) && ends_in_quote (fnbase, 6))
+	{
+		if (fnbase[1] == '\"')
+			fseek (inp, found_at + 2, SEEK_SET);
+
+		if (getc (inp) == ':')
+		{
+			fread (temp, 7, 1, inp);
+			if (not_embedding_bracket(temp))
+			{
+				if (doesnt_have_junk(temp))
+				{
+
+					fseek (inp, found_at, SEEK_SET);
+					while ((c != ']') && (c != '\"'))
+						c = getc (inp);
+					if (c == ']')
+						return 0;
+					while ((c = getc (inp)) != '\"')
+						;
+					while ((c = getc (inp)) != '\"')
+						;
+					while ((c = getc (inp)) != '\"')
+						;
+
+					fread (temp, 7, 1, inp);
+					if (!is_all_ascii (temp))
+						return 0;
+					if (!ends_in_quote (temp, 6))
+						return 0;
+
+					*strchr (fnbase, '\"') = 0;
+					printf ("found tbl @ 0x%lx\t%s\n", found_at, fnbase);
+					if (outp)
+						fprintf (outp, "found tbl @ 0x%lx\t%s\n", found_at, fnbase);
+					fseek (inp, found_at, SEEK_SET);
+					return 1;
+				}
+			}
+		}
+	}
+	fseek (inp, found_at, SEEK_SET);
+	return 0;
+}
+
+void find_media_tables (FILE *inp, FILE *outp)
+{
+	unsigned char c=0, old_c=0, old_old_c=0;
+
+	while (!feof(inp))
+	{
+		c = getc(inp);
+		if ((c == '\"') && (old_c == '[') && (old_old_c != 0x20))
+		{
+			if (!check_for_table(inp, outp))
+				check_for_captions(inp, outp);
+		}
+		old_old_c = old_c;
+		old_c = c;
+	}
+}
+
 void save_match (FILE *inp, FILE *outp)
 {
 	int i;
@@ -152,6 +310,9 @@ int main (int argc, char *argv[])
 	}
 	st_force_unknown_file (1);
 	st_set_filename (filename);
+	printf ("Scanning for entries...\n");
+	if (save_file)
+		fprintf (outp, "Scanning for entries...\n");
 	do
 	{
 		returned = st_find_start (inp);
@@ -187,9 +348,9 @@ int main (int argc, char *argv[])
 		}
 	}
 	while (returned);
-	printf ("Found:\n");
+	printf ("Found entries:\n");
 	if (save_file)
-		fprintf (outp, "Found:\n");
+		fprintf (outp, "Found entries:\n");
 	for (i = 0; i < 3; i++)
 	{
 		printf ("\t%s\t%d\n", sections[i], counts[i]);
@@ -197,8 +358,16 @@ int main (int argc, char *argv[])
 			fprintf (outp, "\t%s\t%d\n", sections[i], counts[i]);
 	}
 
+	rewind (inp);
+
+	printf ("Scanning for media lists...\n");
+	if (save_file)
+		fprintf (outp, "Scanning for media lists...\n");
+
+	find_media_tables (inp, outp);
+
 	if (outp)
 		fclose (outp);
-
+	fclose (inp);
 	return 0;
 }
