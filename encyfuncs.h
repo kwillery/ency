9bde3dc798adffae7eda173609d011d55bf7fc3f
Robert Mibus <mibus@mibus.org>
2000-10-30 11:34:11 +0000

    add encyfuncs.h

---
+++ encyfuncs.h
@@ -0,0 +1,14 @@
+#ifndef ENCYFUNCS_H
+#define ENCYFUNCS_H
+
+#include "ency.h"
+
+/* internal */
+#define ST_SECT_PTBL 10
+#define ST_SECT_VTBL 11
+#define ST_SECT_PCPT 12
+#define ST_SECT_VCPT 13
+
+int curr_open (long start);
+
+#endif
