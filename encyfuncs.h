40d4668e354cd552e0fbba0e3078f1560b1be4c4
Robert Mibus <mibus@mibus.org>
2003-03-09 04:21:02 +0000

    Export st_return_[fmt|text]().

--- encyfuncs.h
+++ encyfuncs.h
@@ -5,6 +5,9 @@
 
 FILE *curr_open (char *filename, long start);
 FILE *open_file (char *fn, long start);
+struct st_ency_formatting *st_return_fmt (FILE *inp);
+char *st_return_text (FILE *inp);
+
 
 #define free(A) {if (A) free (A); A=NULL;}
 #ifdef DEBUG
